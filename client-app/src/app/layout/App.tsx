ab9ad51ff8994a99505cbb4ef6acc1bfdca8873e
Edmir Tavares <edmirtav@gmail.com>
2020-03-21 22:46:49 -0100

    commit ap activites- session 4

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div >
+      <Header as='h2' icon='users' content='Reactivities' />
+      sss
+    </div>
+  );
+}
+
+export default App;
