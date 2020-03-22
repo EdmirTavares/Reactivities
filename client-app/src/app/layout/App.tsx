9ad5d8a04f7a92d06b850062a6ef2d112dc77c26
Edmir Tavares <edmirtav@gmail.com>
2020-03-22 16:35:09 -0100

    and of session 5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
-import { Header } from 'semantic-ui-react'
+import React, { useState, useEffect, Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+import { NavBar } from '../../fatures/nav/NavBar';
+import { ActivityDashboard } from '../../fatures/activities/dashboard/ActivityDashboard';
 
-function App() {
+
+
+const App = () => {
+
+  const [activities, setActivities] = useState<IActivity[]>([])
+  const [selectedActivity, setselectedActivity] = useState<IActivity | null>(null)
+  const [editMode, setEditMode] = useState(false)
+
+  const handleSelectActivity = (id: string) => {
+    setselectedActivity(activities.filter(a => a.id === id)[0])
+    setEditMode(false);
+  }
+
+  const handleOpenCreateForm = () => {
+    setselectedActivity(null);
+    setEditMode(true);
+  }
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setselectedActivity(activity);
+    setEditMode(false);
+
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    setselectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)])
+  }
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+      let activities:IActivity[] = []
+
+      response.data.forEach( activity => {
+        activity.date = activity.date.split('.')[0]
+        activities.push(activity)
+      })
+        setActivities(activities)
+      });
+  }, []);
+
   return (
-    <div >
-      <Header as='h2' icon='users' content='Reactivities' />
-      sss
-    </div>
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setselectedActivity={setselectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </Fragment>
+
   );
+
 }
 
 export default App;
