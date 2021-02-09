8a2a99b5054d6ffe935c3a2b3a47ebf99fa95f01
edmir.tavares <edmirtav@gmail.com>
2021-02-09 07:42:37 -0100

    End of session 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,38 +1,67 @@
-import React, { Fragment } from 'react';
-import { Container } from 'semantic-ui-react';
-import NavBar from '../../fatures/nav/NavBar';
-import ActivityDashboard from '../../fatures/activities/dashboard/ActivityDashboard'
-import { observer } from 'mobx-react-lite'
-import { Route, withRouter, RouteComponentProps, Switch } from 'react-router-dom';
-import HomePage from '../../fatures/home/HomePage';
-import ActivityForm from '../../fatures/activities/form/ActivityForm';
-import ActivityDetails from '../../fatures/activities/details/ActivityDetails';
-import NotFound from './NotFound';
-import { ToastContainer } from 'react-toastify';
+import React, { Fragment, useContext, useEffect } from "react";
+import { Container } from "semantic-ui-react";
+import NavBar from "../../fatures/nav/NavBar";
+import ActivityDashboard from "../../fatures/activities/dashboard/ActivityDashboard";
+import { observer } from "mobx-react-lite";
+import {
+  Route,
+  withRouter,
+  RouteComponentProps,
+  Switch,
+} from "react-router-dom";
+import HomePage from "../../fatures/home/HomePage";
+import ActivityForm from "../../fatures/activities/form/ActivityForm";
+import ActivityDetails from "../../fatures/activities/details/ActivityDetails";
+import NotFound from "./NotFound";
+import { ToastContainer } from "react-toastify";
+import LoginForm from "../../fatures/user/LoginForm";
+import { RootStoreContext } from "../stores/rootStore";
+import LoadingComponent from "./LoadingComponent";
+import ModalContainer from "../common/modals/ModalContainer";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { setAppLoaded, token, appLoaded } = rootStore.commonStore;
+  const { getUser } = rootStore.userStore;
+
+  useEffect(() => {
+    if (token) {
+      getUser().finally(() => setAppLoaded());
+    } else {
+      setAppLoaded();
+    }
+  }, [getUser, setAppLoaded, token]);
+
+  if (!appLoaded) return <LoadingComponent content="loading app" />;
 
   return (
     <Fragment>
-      <ToastContainer position='bottom-right' />
-      <Route exact path='/' component={HomePage} />
-      <Route path={'/(.+)'} render={() => (
-        <Fragment>
-          <NavBar />
-          <Container style={{ marginTop: '7em' }}>
-            <Switch>
-              <Route exact path='/activities' component={ActivityDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-              <Route component={NotFound} />
-            </Switch>
-          </Container>
-        </Fragment>
-      )} />
+      <ModalContainer />
+      <ToastContainer position="bottom-right" />
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route path="/login" component={LoginForm} />
+                <Route component={NotFound} />
+              </Switch>
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
-
   );
-
-}
+};
 
 export default withRouter(observer(App));
